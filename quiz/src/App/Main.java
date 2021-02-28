d82a4339b66d58fd96faeeff64090cdc16066f6f
AxelCarlsson <73167102+AxelCarlsson@users.noreply.github.com>
2021-02-28 16:12:32 +0100

    axel test code

--- quiz/src/App/Main.java
+++ quiz/src/App/Main.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 public class Main {
 
     public static void main (String []args) throws IOException {
+
         Quiz myQuiz = new Quiz();
-        myQuiz.getQuestions();
+        myQuiz.getQuestions("1 ");
+        myQuiz.answerAlternatives("1");
+        myQuiz.getCorrectAnswer("1");
 
     }
 }
--- quiz/src/App/Quiz.java
+++ quiz/src/App/Quiz.java
@@ -3,34 +3,49 @@ package App;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Quiz {
     // Klass Quiz
     //Ni ska skapa en klass som håller reda på frågor, svarsalternativ och vilket svarsalternativ som är rätt.
 
-    String questions = "C:\\Users\\Axel\\IdeaProjects\\Quiz\\quiz\\src\\Files\\Questions.txt";
-    Stream<String> temp = Files.lines(Paths.get(questions));
+
 
     public Quiz() throws IOException {
     }
 
 
-    public void answerAlternatives() {
-
-    }
-
-    public void getCorrectAnswer(){
-
-    }
-
-    public void getQuestions() throws IOException {
+    public void answerAlternatives(String alternativeNumber) throws IOException {
+        String questions = "C:\\Users\\Axel\\IdeaProjects\\Quiz\\quiz\\src\\Files\\Questions.txt";
+        Stream<String> tempAnswer = Files.lines(Paths.get(questions));
 
 
-        temp
-                //.filter(x -> )
+        tempAnswer
+                .filter(x -> x.contains(alternativeNumber + "A") || x.contains(alternativeNumber +"B") || x.contains(alternativeNumber +"C") || x.contains(alternativeNumber + "D"))
                 .forEach(x -> System.out.println(x));
-        temp.close();
+        tempAnswer.close();
+
+    }
+
+    public void getCorrectAnswer(String answerNumber) throws IOException {
+        String questions = "C:\\Users\\Axel\\IdeaProjects\\Quiz\\quiz\\src\\Files\\Questions.txt";
+        Stream<String> tempAnswer = Files.lines(Paths.get(questions));
+
+        tempAnswer
+                .filter(x -> x.contains(answerNumber + " " + "ANSWER"))
+                .forEach(x -> System.out.println(x));
+        tempAnswer.close();
+    }
+
+    public void getQuestions(String questionNumber) throws IOException {
+        String questions = "C:\\Users\\Axel\\IdeaProjects\\Quiz\\quiz\\src\\Files\\Questions.txt";
+        Stream<String> tempQuestion = Files.lines(Paths.get(questions));
+
+        tempQuestion
+                .filter(x -> x.contains("?") && x.contains(questionNumber))
+                .forEach(x -> System.out.println(x));
+        tempQuestion.close();
     }
 
     //Den ska också innehålla en metod som ska läsa in de serialiserade frågeobjekten från en fil.
