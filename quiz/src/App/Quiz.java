6cc99aa8c7cc599c6aa2d95e5fe13a97a0064b92
denl <denisee.larsson@gmail.com>
2021-03-01 10:43:29 +0100

    created a new branch

--- quiz/src/App/Quiz.java
+++ quiz/src/App/Quiz.java
@@ -5,12 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
-
 public class Quiz {
     // Klass Quiz
     //Ni ska skapa en klass som håller reda på frågor, svarsalternativ och vilket svarsalternativ som är rätt.
@@ -21,7 +15,8 @@ public class Quiz {
 
 
     public void answerAlternatives(String alternativeNumber) throws IOException {
-        String questions = "C:/Users/Axel/IdeaProjects/Quiz/quiz/src/Files/Questions.txt";
+        //String questions = "C:/Users/Axel/IdeaProjects/Quiz/quiz/src/Files/Questions.txt";
+        String questions = "C:/Workspace/Quiz/quiz/src/Files/Questions";
         Stream<String> tempAnswer = Files.lines(Paths.get(questions));
 
 
