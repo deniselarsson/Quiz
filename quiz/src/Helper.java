495c0f07d5b73e0b7a4d1f54ea35bd152989653c
denl <denisee.larsson@gmail.com>
2021-03-07 22:55:32 +0100

    denise test

--- quiz/src/Helper.java
+++ quiz/src/Helper.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 
 public class Helper {
 
-    //Skriver ut text i konsolen
     public static void printText (String text) {
         System.out.println(text);
     }
 
-    //Spara det som skrivs in från konsolen
     public static String scanInput () {
 
         Scanner input = new Scanner(System.in);
