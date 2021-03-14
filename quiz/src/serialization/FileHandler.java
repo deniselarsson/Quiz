1fc32078591896e6346f79b6f01fe7bedd5fae02
denl <denisee.larsson@gmail.com>
2021-03-14 23:51:25 +0100

    denise serializering synkad med axels branch

--- quiz/src/serialization/FileHandler.java
+++ quiz/src/serialization/FileHandler.java
@@ -3,21 +3,18 @@ package serialization;
 import java.io.*;
 import java.util.ArrayList;
 
-
 public class FileHandler {
 
-    //Skicka in en fil questions.txt som serializera
-    //läser från filen in i minnet
+    //Läser in filen till minnet
     private static Object read (String fileName) throws IOException, ClassNotFoundException {
 
         ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName));
-        ;
         Object result = in.readObject();
         in.close();
         return result;
     }
 
-    //skriver ut från minnet
+    //Skriver ut objektet  från minnet till en fil
     private static void write (String fileName, Object object) {
 
         try {
@@ -31,13 +28,12 @@ public class FileHandler {
         }
     }
 
-    // läser in till questions list
+    //Läser in frågor från user till filen
     public static ArrayList<Question> readQuestions () {
 
         try {
             return (ArrayList<Question>) read("questions.txt");
         }
-        //om inte filen finns så returnera den en tom lista
         catch (IOException e) {
             return new ArrayList<>();
         }
@@ -45,23 +41,9 @@ public class FileHandler {
             return new ArrayList<>();
         }
     }
-    //den skriver input från console till arraylist question
+
+    //Skriver frågorna till filen
     public static void writeQuestions (ArrayList<Question> question) {
         write("questions.txt", question);
-    }
-
-    // läser in till questions list
-    public static ArrayList<Option> readOption () {
-
-        try {
-            return (ArrayList<Option>) read("questions.txt");
-        }
-        //om inte filen finns så returnera den en tom lista
-        catch (IOException e) {
-            return new ArrayList<>();
-        }
-        catch (ClassNotFoundException e) {
-            return new ArrayList<>();
-        }
     }
 }
---
+++ quiz/src/serialization/MainTest.java
@@ -0,0 +1,12 @@
+package serialization;
+
+public class MainTest {
+
+    public static void main (String []args){
+
+
+       Question.setNewQuestion();
+       Question.printAllQuestions(FileHandler.readQuestions());
+       Question.printOneQuestion(FileHandler.readQuestions());
+    }
+}
