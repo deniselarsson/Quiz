9e3e0aa7e89092a382f132be0789a8cebb1d567b
denl <denisee.larsson@gmail.com>
2021-03-14 23:54:32 +0100

    denise serializering synkad med axels branch

--- quiz/src/serialization/Option.java
+++ quiz/src/serialization/Option.java
@@ -5,6 +5,8 @@ public class Option {
     String text;
     boolean isCorrectAnswer;
 
+    //för att organisera options
+    //Måste finnas en text och en boolean
     Option(String text, boolean isCorrectAnswer){
         this.text = text;
         this.isCorrectAnswer = isCorrectAnswer;
